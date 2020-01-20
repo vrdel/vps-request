6dbaffc41f9ad66773925615d526688b5c5be9e5
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-01-20 13:14:14 +0100

    initial DB helper tool

--- docker/docker-run-vpsrequest.sh
+++ docker/docker-run-vpsrequest.sh
@@ -18,5 +18,6 @@ docker run --privileged --rm --name vps-request -ti -p 80:80 -p 443:443 -p 8000:
 --log-driver json-file --log-opt max-size=10m \
 -v /dev/log/:/dev/log \
 -v $WORKDIR/git.vps-request/vps-request/vpsrequest/:$VENV/lib64/python3.5/site-packages/vpsrequest \
+-v $WORKDIR/git.vps-request/vps-request/bin/vpsreq-db:$VENV/bin/vpsreq-db \
 $IMG \
 /bin/bash
