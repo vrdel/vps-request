5fd877f89e0690018f5cf693b25a2126fc88ffe3
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-01-26 18:55:54 +0100

    load Django project dir into sys.path

--- vpsrequest/vpsrequest/wsgi.py
+++ vpsrequest/vpsrequest/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from distutils.sysconfig import get_python_lib
 from django.core.wsgi import get_wsgi_application
 
+sys.path.insert(0, get_python_lib() + '/vpsrequest/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vpsrequest.settings')
 
 application = get_wsgi_application()
