0b686ec9a9254705ec1de36f2609a0cdac98c0b2
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-01-29 15:53:34 +0100

    support for bundle build from container

--- docker/collectstatic.sh
+++ docker/collectstatic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. /data/vps-request/bin/activate
 rm -rf $VIRTUAL_ENV/share/vpsrequest/static/reactbundle/* ; \
 vpsreq-manage collectstatic --noinput ; \
 rm -rf $VIRTUAL_ENV/share/vpsrequest/static/admin
