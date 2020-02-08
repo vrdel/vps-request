886fb2d942cba57d606ddeaff6e380913321b9d1
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-02-08 14:54:57 +0100

    add Profile model as extension of User

--- vpsrequest/backend/models.py
+++ vpsrequest/backend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    class Meta:
+        app_label = 'backend'
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    aaieduhr = models.CharField(
+        'hrEduPersonUniqueID',
+        max_length=256,
+        blank=True,
+        null=True,
+    )
+
+    institution = models.CharField(
+        'Institution',
+        max_length=256,
+        blank=True,
+        null=True
+    )
+
+    role = models.CharField(
+        'Role',
+        max_length=64,
+        blank=True,
+        null=True
+    )
