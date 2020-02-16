8da83b02128e88a801d3585d0045da196cfa6b78
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-02-16 21:15:10 +0100

    generate primary key ids for users starting from passed arg

--- helpers/db-import.py
+++ helpers/db-import.py
@@ -7,7 +7,10 @@ import json
 def main():
     parser = argparse.ArgumentParser(description="DB import from MySQL DB JSON dump")
     parser.add_argument('-f', required=True, help='MySQL DB JSON dump', dest='dump')
-    parser.add_argument('-o', required=True, help='reformatted DB JSON dump', dest='out')
+    parser.add_argument('-o', required=True, help='Reformatted DB JSON dump', dest='out')
+    parser.add_argument('--skip-user', type=int, required=True,
+                        help='Skip number of users already in DB',
+                        dest='numskipusers')
     args = parser.parse_args()
 
     user_map = {
@@ -23,7 +26,7 @@ def main():
     users = list()
     requests = list()
     num_request = 1
-    num_user = 2
+    num_user = args.numskipusers + 1
     looked_users = set()
 
     with open(args.dump) as fp:
