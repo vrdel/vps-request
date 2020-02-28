3905a4741159300e102e21042f875131918cd0e8
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-02-28 17:35:14 +0100

    initial Django CLI tool for creating users

--- vpsrequest/backend/management/commands/superuser.py
+++ vpsrequest/backend/management/commands/superuser.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from django.db import DEFAULT_DB_ALIAS, connection
+from django.db import DEFAULT_DB_ALIAS
 from django.db.utils import IntegrityError
-
-from configparser import ConfigParser
 
 # based on django/contrib/auth/management/commands/createsuperuser.py
 
@@ -15,19 +13,19 @@ class Command(BaseCommand):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.UserModel = get_user_model()
+        self.user_model = get_user_model()
 
     def handle(self, *args, **kwargs):
         user_data = dict()
 
-        user_data[self.UserModel.USERNAME_FIELD] = settings.SUPERUSER_NAME
+        user_data[self.user_model.USERNAME_FIELD] = settings.SUPERUSER_NAME
         user_data['first_name'] = settings.SUPERUSER_NAME
         user_data['last_name'] = settings.SUPERUSER_NAME
         user_data['password'] = settings.SUPERUSER_PASS
         user_data['email'] = settings.SUPERUSER_EMAIL
 
         try:
-            user = self.UserModel._default_manager.db_manager(DEFAULT_DB_ALIAS).create_superuser(**user_data)
+            user = self.user_model._default_manager.db_manager(DEFAULT_DB_ALIAS).create_superuser(**user_data)
             user.aaieduhr = None
             user.institution = 'SRCE'
             user.role = 'Superuser'
