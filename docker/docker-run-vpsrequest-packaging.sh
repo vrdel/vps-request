6034f0374643ed48f3804b4138ec4954e5de344f
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-04-25 22:08:21 +0200

    container for packaging test

---
+++ docker/docker-run-vpsrequest-packaging.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IMAGE="ipanema:5000/vps-request-packaging"
+VENV=/data/vps-request
+WORKDIR=$HOME/my_work/srce/git.vps-request/vps-request/
+
+if [ -z "$1" ]
+then
+	IMG="$IMAGE"
+else
+	IMG="$IMAGE:$1"
+fi
+
+docker run --net vrdel-net --ip 172.18.0.10 --privileged --rm --name vps-request-packaging -ti \
+	-p 80:80 -p 443:443 -p 3306:3306 -p 8000:8000 \
+-e ZDOTDIR=/mnt \
+-v $HOME:/mnt/ \
+-v /etc/localtime:/etc/localtime:ro \
+--log-driver json-file --log-opt max-size=10m \
+-v /dev/log/:/dev/log \
+-v $WORKDIR/:/home/user/vpsrequest-source \
+-v $WORKDIR/docker/safety.sh:/home/user/safety.sh \
+-v $WORKDIR/docker/collectstatic.sh:/home/user/collectstatic.sh \
+-v $WORKDIR/docker/restarthttpd.sh:/home/user/restarthttpd.sh \
+-v $WORKDIR/docker/syncsite.sh:/home/user/syncsite.sh \
+-v $WORKDIR/docker/pysitepkg:/home/user/pysitepkg \
+$IMG \
+/bin/bash
