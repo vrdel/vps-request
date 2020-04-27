d85fe3a12b0ff09408e0a5c9b4bdc145c3704b2a
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-04-27 15:48:51 +0200

    add relative path for SAML2 login endpoints

--- vpsrequest/backend/saml2/config.py
+++ vpsrequest/backend/saml2/config.py
@@ -10,12 +10,13 @@ from django.conf import settings
 def get_hostname(request):
     return request.get_host().split(':')[0]
 
+
 def get_saml_config(request):
     hostname = get_hostname(request)
 
     config = {
         'xmlsec_binary': '/usr/bin/xmlsec1',
-        'entityid': 'https://%s/saml2/metadata/' % hostname,
+        'entityid': 'https://{}{}/saml2/metadata/'.format(hostname, settings.RELATIVE_PATH),
         'allow_unknown_attributes': 'true',
         'debug': 1,
         'service': {
@@ -24,16 +25,15 @@ def get_saml_config(request):
                 'want_assertions_signed': 'true',
                 'endpoints': {
                     'assertion_consumer_service': [
-                        ('https://%s/saml2/acs/' % hostname,
+                        ('https://{}{}/saml2/acs/'.format(hostname, settings.RELATIVE_PATH),
                          saml2.BINDING_HTTP_POST),
                     ],
                     'single_logout_service': [
-                        ('https://%s/saml2/ls/' % hostname,
+                        ('https://{}{}/saml2/ls/'.format(hostname, settings.RELATIVE_PATH),
                          saml2.BINDING_HTTP_REDIRECT),
                     ],
                 },
-                'attribute_map_dir': '%s/saml2/attributemaps/' %
-                                     get_python_lib(),
+                'attribute_map_dir': '{}/saml2/attributemaps/'.format(get_python_lib()),
             },
         },
         'key_file': settings.HOST_KEY,  # private part
