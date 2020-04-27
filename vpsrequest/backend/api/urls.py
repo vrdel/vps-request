97ae3e4cf5dc07c11101cb563566d4df51645fee
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-04-27 10:47:01 +0200

    don't need to run admin tools as a root

--- vpsrequest/backend/api/urls.py
+++ vpsrequest/backend/api/urls.py
@@ -6,5 +6,4 @@ app_name = 'backend'
 
 urlpatterns = [
     path('sessionactive/', views.IsSessionActive.as_view(), name='sessionactive'),
-    path('saml2login/', views.Saml2Login.as_view(), name='saml2login'),
 ]
