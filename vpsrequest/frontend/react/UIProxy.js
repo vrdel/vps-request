9b4fea36b426d7b03ad7ff2326f66194571aa5e2
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-05-10 04:00:00 +0200

    smaller logos on UIProxy

--- vpsrequest/frontend/react/UIProxy.js
+++ vpsrequest/frontend/react/UIProxy.js
@@ -8,7 +8,6 @@ import {
 } from 'reactstrap';
 import './Login.css';
 import CloudLogoBig from './logos/logo_cloud-big.png';
-import SrceLogoBig from './logos/logo_srce-big.png';
 import { RelativePath } from './Config';
 
 export const UIProxy = ({redirect=false}) =>
@@ -21,22 +20,17 @@ export const UIProxy = ({redirect=false}) =>
   return (
     <Container>
       <Row className="login-first-row">
-        <Col sm={{size: 8, offset: 2}}>
-          <Card>
+        <Col sm={{size: 6, offset: 3}}>
+          <Card body outline color="primary">
             <CardBody>
               <div className="text-center pt-2 pb-2">
                 <img src={CloudLogoBig} id="cloud logo" alt="VPS Cloud Logo"/>
               </div>
-              <div className="text-center mt-5 mb-5">
+              <div className="text-center mt-5">
                 <a href="https://www.srce.unizg.hr/cloud/vps">
-                  <h1>
+                  <h2>
                     https://www.srce.unizg.hr/cloud/vps
-                  </h1>
-                </a>
-              </div>
-              <div className="text-center pt-2 pb-2">
-                <a href="https://www.srce.unizg.hr/" target="_blank" rel="noopener noreferrer">
-                  <img src={SrceLogoBig} id="srcelogo" alt="SRCE Logo"/>
+                  </h2>
                 </a>
               </div>
             </CardBody>
