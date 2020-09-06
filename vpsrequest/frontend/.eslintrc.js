14f6015e3db4a346d38340a6a4d7f8a98a3201e1
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-09-06 09:42:51 +0200

    enhanced linter

--- vpsrequest/frontend/.eslintrc.js
+++ vpsrequest/frontend/.eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": [ "eslint:recommended", "plugin:react/recommended"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended" ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -16,11 +18,17 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "react-hooks"
     ],
     "rules": {
+        "react/prop-types": [0],
         "react/jsx-uses-react": "error",
         "react/jsx-uses-vars": "error",
-        "react/prop-types": [0]
+        "react/display-name": [0, { "ignoreTranspilerName": false }],
+        "react/jsx-indent": [2, 2, {checkAttributes: true}],
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+
     }
 };
