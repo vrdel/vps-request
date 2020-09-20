2d6b36d199c6a6b6d72ddb7943f52f6cf7937d7c
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-09-20 22:30:03 +0200

    custom primary and success colors

--- vpsrequest/frontend/react/index.js
+++ vpsrequest/frontend/react/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap-custom.scss'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('react'));
 // If you want your app to work offline and load faster, you can change
