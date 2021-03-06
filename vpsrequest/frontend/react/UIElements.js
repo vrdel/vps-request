e81bae79eab50d77e3934471c605cdd0e72c23c0
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-03-06 01:06:38 +0100

    have dropdown in navbar for ordinary user pages

--- vpsrequest/frontend/react/UIElements.js
+++ vpsrequest/frontend/react/UIElements.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
   Button,
   Card,
   CardBody,
@@ -30,6 +34,7 @@ import {
   faFileAlt,
   faFileSignature,
   faHandshake,
+  faUserEdit,
   faThumbsDown,
   faBatteryHalf,
 } from '@fortawesome/free-solid-svg-icons';
@@ -45,12 +50,16 @@ import { Helmet } from "react-helmet";
 import './UIElements.css';
 
 
-var listPages = ['novi-zahtjevi', 'odobreni-zahtjevi', 'odbijeni-zahtjevi', 'novi-zahtjev', 'stanje-zahtjeva'];
+var staffPages = ['novi-zahtjevi', 'odobreni-zahtjevi', 'odbijeni-zahtjevi',
+                  'predumirovljenje'];
+var noStaffPages = ['aktivni-posluzitelji',  'novi-zahtjev', 'stanje-zahtjeva']
 
 var linkTitle = new Map();
 linkTitle.set('novi-zahtjevi', 'Novi zahtjevi');
 linkTitle.set('odobreni-zahtjevi', 'Odobreni zahtjevi');
 linkTitle.set('odbijeni-zahtjevi', 'Odbijeni zahtjevi');
+linkTitle.set('predumirovljenje', 'Predumirovljenje');
+linkTitle.set('aktivni-posluzitelji', 'Aktivni VM-ovi');
 linkTitle.set('novi-zahtjev', 'Zahtjev za novim VM-om');
 linkTitle.set('stanje-zahtjeva', 'Stanje zahtjeva');
 
@@ -62,6 +71,8 @@ export const Icon = props => {
   linkIcon.set('odbijeni-zahtjevi', faThumbsDown);
   linkIcon.set('novi-zahtjev', faFileSignature);
   linkIcon.set('stanje-zahtjeva', faBatteryHalf);
+  linkIcon.set('predumirovljenje', faBatteryHalf);
+  linkIcon.set('aktivni-posluzitelji', faBatteryHalf);
 
   return <FontAwesomeIcon
             icon={linkIcon.get(props.i)}
@@ -239,25 +250,52 @@ const NavigationBar = ({history, onLogout, isOpenModal, toggle, titleModal,
 
 
 const NavigationLinks = ({location, isStaff, canApproveRequest}) => {
-  var noStaffPages = ['novi-zahtjev', 'stanje-zahtjeva']
-  var pages = listPages
-
-  if (!isStaff && !canApproveRequest)
-    pages = noStaffPages
+  var pages = staffPages
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen(!dropdownOpen);
 
   return (
-    <Nav tabs id="vpsreq-navlinks" className="d-flex justify-content-center border-left border-right border-top rounded-top sticky-top pl-3 pr-3">
+    <Nav tabs id="vpsreq-navlinks" className="d-flex justify-content-center border-left border-right border-top rounded-top sticky-top">
       {
-        pages.map((item, i) =>
-          <NavItem key={i} className='mt-1 mr-2'>
-            <NavLink
-              tag={Link}
-              active={location.pathname.split('/')[2] === item ? true : false}
-              className={location.pathname.split('/')[2] === item ? "text-white bg-info" : "text-dark"}
-              to={'/ui/' + item}><Icon i={item}/> {linkTitle.get(item)}
-            </NavLink>
-          </NavItem>
-        )
+        !isStaff && !canApproveRequest ?
+          noStaffPages.map((item, i) =>
+            <NavItem key={i} className='mt-1'>
+              <NavLink
+                tag={Link}
+                active={location.pathname.split('/')[2] === item ? true : false}
+                className={location.pathname.split('/')[2] === item ? "text-white bg-info" : "text-dark"}
+                to={'/ui/' + item}><Icon i={item}/> {linkTitle.get(item)}
+              </NavLink>
+            </NavItem>)
+        :
+          <React.Fragment>
+            {
+              staffPages.map((item, i) =>
+                <NavItem key={i} className='mt-1'>
+                  <NavLink
+                    tag={Link}
+                    active={location.pathname.split('/')[2] === item ? true : false}
+                    className={location.pathname.split('/')[2] === item ? "text-white bg-info" : "text-dark"}
+                    to={'/ui/' + item}><Icon i={item}/> {linkTitle.get(item)}
+                  </NavLink>
+                </NavItem>)
+            }
+            <Dropdown nav isOpen={dropdownOpen} toggle={toggle} className='mt-1 text-dark'>
+              <DropdownToggle nav caret className={dropdownOpen ? "text-white bg-info" : "text-dark"}>
+                <FontAwesomeIcon icon={faUserEdit}/> Korisnik
+              </DropdownToggle>
+              <DropdownMenu>
+                {
+                  noStaffPages.map((item, i) =>
+                    <DropdownItem key={i} id='vpsreq-navlinks-dropdown'>
+                      <Link id='vpsreq-navlinks-dropdown' to={'/ui/' + item} className="text-dark">
+                        <Icon i={item}/> {linkTitle.get(item)}
+                      </Link>
+                    </DropdownItem>)
+                }
+              </DropdownMenu>
+            </Dropdown>
+          </React.Fragment>
       }
     </Nav>
   )
