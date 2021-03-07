958f7b88cbad19781be8f6f14871952df087a784
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-03-07 19:14:22 +0100

    have Alert as part of BaseView

--- vpsrequest/frontend/react/RequestsActiveMy.js
+++ vpsrequest/frontend/react/RequestsActiveMy.js
@@ -61,8 +61,6 @@ const MyRequestsActive = (props) => {
   const [requestsData, setRequests] = useState(undefined);
   const [alertVisible, setAlertVisible] = useState(true);
 
-  const alertDismiss = () => setAlertVisible(false);
-
   const initializeComponent = async () => {
     const session = await backend.isActiveSession();
 
@@ -120,99 +118,90 @@ const MyRequestsActive = (props) => {
     return (
       <BaseView
         title='Aktivni poslužitelji'
-        location={location}>
-        <Row>
-          <Col md={{size: 6, offset: 3}}>
-            {
-              <Alert color="danger" isOpen={shouldAskVMActive && alertVisible} toggle={alertDismiss}>
-                izjasni se {userDetails.vmisactive_responsedate}
-              </Alert>
-            }
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Formik
-              initialValues={{activeRequests: emptyIfNullRequestPropery(requestsData)}}
-              validationSchema={RequestsActiveSchema}
-              validateOnChange={false}
-              validateOnBlur={false}
-              onSubmit={(values, {setSubmitting} )=> {
-                handleOnSubmit(values.activeRequests)
-                setSubmitting(false)
-              }}
-            >
-              {props => (
-                <Form>
-                  <FieldArray
-                    name="activeRequests"
-                    render={() => (
-                      <React.Fragment>
-                        <Row>
-                          <Table responsive hover size="sm" className={`${shouldAskVMActive && alertVisible ? "mt-1" : "mt-4"}`}>
-                            <thead className="table-active align-middle text-center">
-                              <tr>
-                                <th style={{width: '90px'}}>Status</th>
-                                <th style={{width: '180px'}}>Datum podnošenja</th>
-                                <th style={{width: '250px'}}>Poslužitelj</th>
-                                <th>Komentar (opcionalno)</th>
-                                <th style={{width: '140px'}}>Potreban u {new Date().getFullYear()}.</th>
-                              </tr>
-                            </thead>
-                            <tbody className="align-middle text-center">
-                              {
-                                props.values.activeRequests.map((request, index) =>
-                                  <tr key={index}>
-                                    <td className="align-middle text-center">
-                                      <Status params={CONFIG['status'][request.approved]} renderToolTip={true}/>
-                                    </td>
-                                    <td className="align-middle text-center">
-                                      { DateFormatHR(request.request_date) }
-                                    </td>
-                                    <td className="align-middle text-center">
-                                      { request.vm_fqdn }
-                                    </td>
-                                    <td className="align-middle text-center">
-                                      <Field
-                                        className="form-control"
-                                        name={`activeRequests.${index}.vm_isactive_comment`}
-                                        as="textarea"
-                                        rows={1}
-                                      />
-                                    </td>
-                                    <td className="align-middle text-center">
-                                      <Field
-                                        name={`activeRequests.${index}.vm_isactive`}
-                                        component={DropDownMyActive}
-                                        data={['Odaberi', 'Da', 'Ne']}
-                                      />
-                                      {
-                                        props.errors && props.errors.activeRequests
-                                          && props.errors.activeRequests[index] ?
-                                            <span className="text-danger" style={{fontSize: 'small'}}>
-                                              {props.errors.activeRequests[index].vm_isactive}
-                                            </span >
-                                          : null
-                                      }
-                                    </td>
-                                  </tr>)
-                              }
-                            </tbody>
-                          </Table>
-                        </Row>
-                        <Row className="mt-2 mb-2 text-center">
-                          <Col>
-                            <Button className="btn-lg" color="success" id="submit-button" type="submit">Spremi</Button>
-                          </Col>
-                        </Row>
-                      </React.Fragment>
-                    )}
-                  />
-                </Form>
-              )}
-            </Formik>
-          </Col>
-        </Row>
+        location={location}
+        alert={alertVisible && shouldAskVMActive}
+        alertdismiss={() => setAlertVisible(false)}
+        alertmsg={`izjasni se ${userDetails.vmisactive_responsedate}`}
+      >
+        <Formik
+          initialValues={{activeRequests: emptyIfNullRequestPropery(requestsData)}}
+          validationSchema={RequestsActiveSchema}
+          validateOnChange={false}
+          validateOnBlur={false}
+          onSubmit={(values, {setSubmitting} )=> {
+            handleOnSubmit(values.activeRequests)
+            setSubmitting(false)
+          }}
+        >
+          {props => (
+            <Form>
+              <FieldArray
+                name="activeRequests"
+                render={() => (
+                  <React.Fragment>
+                    <Row>
+                      <Table responsive hover size="sm" className={`${shouldAskVMActive && alertVisible ? "mt-1" : "mt-4"}`}>
+                        <thead className="table-active align-middle text-center">
+                          <tr>
+                            <th style={{width: '90px'}}>Status</th>
+                            <th style={{width: '180px'}}>Datum podnošenja</th>
+                            <th style={{width: '250px'}}>Poslužitelj</th>
+                            <th>Komentar (opcionalno)</th>
+                            <th style={{width: '140px'}}>Potreban u {new Date().getFullYear()}.</th>
+                          </tr>
+                        </thead>
+                        <tbody className="align-middle text-center">
+                          {
+                            props.values.activeRequests.map((request, index) =>
+                              <tr key={index}>
+                                <td className="align-middle text-center">
+                                  <Status params={CONFIG['status'][request.approved]} renderToolTip={true}/>
+                                </td>
+                                <td className="align-middle text-center">
+                                  { DateFormatHR(request.request_date) }
+                                </td>
+                                <td className="align-middle text-center">
+                                  { request.vm_fqdn }
+                                </td>
+                                <td className="align-middle text-center">
+                                  <Field
+                                    className="form-control"
+                                    name={`activeRequests.${index}.vm_isactive_comment`}
+                                    as="textarea"
+                                    rows={1}
+                                  />
+                                </td>
+                                <td className="align-middle text-center">
+                                  <Field
+                                    name={`activeRequests.${index}.vm_isactive`}
+                                    component={DropDownMyActive}
+                                    data={['Odaberi', 'Da', 'Ne']}
+                                  />
+                                  {
+                                    props.errors && props.errors.activeRequests
+                                      && props.errors.activeRequests[index] ?
+                                        <span className="text-danger" style={{fontSize: 'small'}}>
+                                          {props.errors.activeRequests[index].vm_isactive}
+                                        </span >
+                                      : null
+                                  }
+                                </td>
+                              </tr>)
+                          }
+                        </tbody>
+                      </Table>
+                    </Row>
+                    <Row className="mt-2 mb-2 text-center">
+                      <Col>
+                        <Button className="btn-lg" color="success" id="submit-button" type="submit">Spremi</Button>
+                      </Col>
+                    </Row>
+                  </React.Fragment>
+                )}
+              />
+            </Form>
+          )}
+        </Formik>
       </BaseView>
     )
   }
